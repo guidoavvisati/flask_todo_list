bb7a66d3b38d078a89e46a9ae7fda3d45aea37e4
Avvisati Guido (GT MO RM) <guido.avvisati@enel.com>
2021-08-27 14:40:06 +0200

    dynamic template

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html", name="GSpot")
