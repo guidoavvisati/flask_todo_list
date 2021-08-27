5848440a435957a2fa70367759c7d043813052a9
Avvisati Guido (GT MO RM) <guido.avvisati@enel.com>
2021-08-27 15:34:13 +0200

    dynamic table content from built-in db

--- app/database.py
+++ app/database.py
@@ -3,3 +3,11 @@ import random
 
 def query_name():
     return random.choice(["Alice", "Bob", "Chris", "Dolly"])
+
+
+def fetch_todo():
+    todo_list = [
+        {"id": 1, "task": "Task 1", "status": "In Progress"},
+        {"id": 2, "task": "Task 2", "status": "Todo"},
+    ]
+    return todo_list
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from app import app
-from app.database import query_name
+import app.database as db
 
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", name=query_name())
+    return render_template("index.html", items=db.fetch_todo())
